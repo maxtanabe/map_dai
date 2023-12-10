dc8b13d99945d9976597254902368a0a7bbf8ecf
maxtanabe <makkori1113jp@gmail.com>
2023-12-10 14:03:32 +0900

    Initial commit

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Users;
+
+class UsersController extends Controller{
+  public function index(){
+    return view("users.index");
+  }
+}
